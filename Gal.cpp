e4af7999d216f2c193dfae937bc192cdf9f55e7f
sinarostami <Sinarostami4188.1@gmail.com>
2020-07-26 16:52:55 +0430

    make copy functions

--- Gal.cpp
+++ Gal.cpp
@@ -1,36 +1,43 @@
 #include <iostream>
 #include <cstdio>
 using namespace std;
-#define length(x) sizeof(x)/sizeof(int)
 int n, m, l;
 int *finalWorld;
+char set[] = {'0', '1'};
 
-void evolution(int *world) {
-	int temp[n][m];
-	for (int x = 0; x < n; x++)
-		for (int y = 0; y < m; y++) {
-			int lives = 0;
-			for (int xd = x - 1; xd <= x + 1; xd++)
-				for (int yd = y - 1; yd <= y + 1; yd++) {
-                    if (* (world + (m * ((xd + n) % n)) + ((yd + m) % m )))  // * (world + (m * ((xd + n) % n)) + ((yd + m) % m ))
-						lives++;
-                }
-					
-			if (* (world + x * m + y)) {
-                lives--; 
-                temp[x][y] = (lives==3 || lives==2);
-            } else {
-                temp[x][y] = lives == 3;
-            }
-			
-
-		}
-	for (int x = 0; x < n; x++)
-		for (int y = 0; y < m; y++)
-			* (world + x * m + y) = temp[x][y];
+void arrCpy(int *src, int *dst) {
+    for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+			* (dst + i * m + j) = * (src + i * m + j);
 }
 
-void draw(int *world) {
+void StrToArrCpy(string str, int *arr) {
+    for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
+			* (arr + i * m + j) = str[m * i + j] - 48;
+}
+
+void evolution(int *world) {
+	int newWorld[n][m];
+    int lives;
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++) {
+			lives = 0;
+			for (int i_neighbor = i - 1; i_neighbor <= i + 1; i_neighbor++)
+				for (int j_neighbor = j - 1; j_neighbor <= j + 1; j_neighbor++)
+                    if (* (world + (m * ((i_neighbor + n) % n)) + ((j_neighbor + m) % m ))) 
+						lives++;
+
+			if (* (world + i * m + j)) {
+                lives--; 
+                newWorld[i][j] = (lives == 3 || lives == 2);
+            } else
+                newWorld[i][j] = (lives == 3);
+		}
+	arrCpy(newWorld[0], world);
+}
+
+void drawWorld(int *world) {
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++)
 			cout << (*(world + i*m + j) ? "*" : ".");
@@ -39,7 +46,7 @@ void draw(int *world) {
     printf("\n");
 }
 
-bool isEqual(int *arr1, int *arr2) {
+bool areEqual(int *arr1, int *arr2) {
     for(int i = 0; i < n; i++)
         for(int j = 0; j < m; j++)
             if(*(arr1 + m * i + j) != *(arr2 + m * i + j))
@@ -49,16 +56,13 @@ bool isEqual(int *arr1, int *arr2) {
 
 bool check(string str) {
     int onesCntFinal = 0, onesCntTemp = 0;
-
-    for(int i = 0; i < n * m; i++)
-        if(*(finalWorld + i) == 1) onesCntFinal++;
-    
     int tempWrold[n][m];
-    for(int i = 0; i < n; i++)
-        for(int j = 0; j < m; j++) {
-            tempWrold[i][j] = str[m * i + j] - 48;
-            if(tempWrold[i][j] == 1) onesCntTemp++;
-        }
+    StrToArrCpy(str, tempWrold[0]);
+
+    for(int i = 0; i < n * m; i++) {
+        if(*(finalWorld + i) == 1) onesCntFinal++;
+        if(*(*tempWrold + i) == 1) onesCntTemp++;
+    }
     
     if(onesCntFinal - 2 > onesCntTemp || onesCntTemp > onesCntFinal + 3) 
         return false;
@@ -66,17 +70,16 @@ bool check(string str) {
     for(int i = 0; i < l; i++)
         evolution(tempWrold[0]);
 
-    if(!isEqual(finalWorld, tempWrold[0]) )
+    if(!areEqual(finalWorld, tempWrold[0]) )
         return false;
 
-    for(int i = 0; i < n; i++)
-        for(int j = 0; j < m; j++)
-            tempWrold[i][j] = str[m * i + j] - 48;
-    draw(tempWrold[0]);
+    StrToArrCpy(str, tempWrold[0]);
+
+    drawWorld(tempWrold[0]);
     exit(0);
 }
 
-void printAllKLengthRec(char set[], string prefix, int k) {
+void makePermutatios(string prefix, int k) {
     if (k == 0) { 
         check(prefix);
         return;
@@ -84,35 +87,33 @@ void printAllKLengthRec(char set[], string prefix, int k) {
     for (int i = 0; i < 2; i++) { 
         string newPrefix; 
         newPrefix = prefix + set[i]; 
-        printAllKLengthRec(set, newPrefix, k - 1); 
+        makePermutatios(newPrefix, k - 1); 
     } 
+}
+
+void makeFinalWorld(string str) {
+    int final[n][m];
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < m; j++)
+            final[i][j] = str[m * i + j] == '*' ? 1 : 0;
+    finalWorld = final[0];
 }
 
 int main() {
     cin >> n >> m >> l;
+    int final[n][m];
     string str = "";
     for(int i = 0; i < n; i++) {
         string temp;
         cin >> temp;
         str += temp;
     }
-    for(int i = 0; i < str.size(); i++) {
-        if(str[i] == '*') 
-            str.replace(i, 1, "1");
-        else
-            str.replace(i, 1, "0");
-    }
-    int final[n][m];
     for(int i = 0; i < n; i++)
         for(int j = 0; j < m; j++)
-            final[i][j] = str[m * i + j] - 48;
+            final[i][j] = str[m * i + j] == '*' ? 1 : 0;
     finalWorld = final[0];
 
-
-
-
-    char set[] = {'0', '1'};
-    printAllKLengthRec(set, "", n * m);
+    makePermutatios("", n * m);
     cout << "impossible" << endl;
 	return 0;
 }
