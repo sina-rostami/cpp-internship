1df00a7e8654852c8861a4c69b53af3425197ac8
sinarostami <Sinarostami4188.1@gmail.com>
2020-08-28 14:47:52 +0430

    unique pointer implemented

---
+++ UniquePointer.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+template <typename T> class UniquePointer {
+private:
+  T *obj_ptr;
+
+public:
+  UniquePointer() = default;
+
+  UniquePointer(T *t) : obj_ptr(t) { cout << "unique pointer creating\n"; };
+
+  UniquePointer(const UniquePointer<T> &other)
+      : obj_ptr(new T(*other.obj_ptr)){};
+
+  UniquePointer(UniquePointer<T> &&other)
+      : obj_ptr(nullptr) { // move constructor
+    obj_ptr = other.obj_ptr;
+    other.obj_ptr = nullptr;
+  }
+
+  ~UniquePointer() {
+    if (obj_ptr != nullptr) {
+      cout << "unique pointer deleting\n";
+      delete obj_ptr;
+      obj_ptr = nullptr;
+    }
+  }
+
+
+  void reset(T *t) {
+    if(obj_ptr) {
+      delete obj_ptr;
+    }
+    obj_ptr = t;
+  }
+
+  T *release() {
+    T *temp = obj_ptr;
+    obj_ptr = nullptr;
+    return temp;
+  }
+
+
+  void swap(UniquePointer<T> &other) {
+    T *temp = obj_ptr;
+    obj_ptr = other.obj_ptr;
+    other.obj_ptr = temp;
+  }
+
+  UniquePointer<T> &operator=(UniquePointer<T> &&other) { // move assignment
+    if (this == &other) // self assingment
+      return *this;
+
+    delete obj_ptr;
+    obj_ptr = other.obj_ptr;
+    other.obj_ptr = nullptr;
+    return *this;
+  }
+
+  /*
+    UniquePointer<T> &operator=(const UniquePointer<T> &other) {
+      if (this == &other) // self assingment
+        return *this;
+
+      delete obj_ptr;
+      obj_ptr = new T(*other.obj_ptr);
+      return *this;
+    }
+  */
+
+  T *operator->() { return obj_ptr; }
+
+  T &operator*() { return *obj_ptr; }
+
+  operator bool() { return obj_ptr != nullptr; }
+};
+
+using std::string;
+int main()
+{
+  UniquePointer<string> a(new string("are"));
+  UniquePointer<string> b;
+  cout << b << endl;
+}
